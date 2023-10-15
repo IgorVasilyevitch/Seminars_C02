f809780417415bed59381720ea67949b0ddc3167
igorvasilyevitch <igorvasilyevitch@yandex.ru>
2023-10-15 15:34:52 +0300

    Первая задача

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.Readline());
+if (number >= 100 && number <= 999);
+{
+   System.Console.WtiteLine(number / 10 % 10);
+}
+else
+{
+   System.Console.WriteLine("ОШИБКА! Вы ввели не трехзначное число ") ; 
+}
